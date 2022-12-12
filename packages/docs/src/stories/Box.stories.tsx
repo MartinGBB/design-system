7039c95c7a3dc8f6e96f20b26bcbf4647ab37586
Martin <escorpmartin97@gmail.com>
2022-12-12 15:33:20 -0300

    feat: componente de Texto

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Box, BoxProps } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
@@ -8,7 +8,7 @@ export default {
   args: {
     children: (
       <>
-        <p>Testando Box</p>
+        <Text>Testando Box</Text>
       </>
     ),
   },
