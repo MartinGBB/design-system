090c600269114e7de83b00b6765c482ad7622af3
Martin <escorpmartin97@gmail.com>
2022-12-20 22:48:33 -0300

    feat: controle de avatar

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/MartinGBB.png',
     alt: 'Martin Brazon',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
